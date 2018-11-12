68a0be1ea938f72bb7931efa1ed7753a3f5fbfbc
newbie1988 <312462392@qq.com>
2018-11-12 20:39:17 +0800

    add cycle reference for shared_ptr

--- c11/shared_ptr/main.cpp
+++ c11/shared_ptr/main.cpp
@@ -14,15 +14,65 @@ public:
 	}
 };
 
+class CTestB;
+class CTestA
+{
+public:
+	~CTestA() 
+	{
+		std::cout<<"CTestA::~CTestA()"<<std::endl;
+	}
+	void SetReference(const std::shared_ptr<CTestB>& test)
+	{
+		m_test = test;
+	}
+
+private:
+	std::shared_ptr<CTestB> m_test;
+};
+
+class CTestB
+{
+public:
+	~CTestB()
+	{
+		std::cout<<"CTestB::~CTestB()"<<std::endl;
+	}
+	void SetReference(const std::shared_ptr<CTestA>& test)
+	{
+		m_test = test;
+	}
+private:
+	std::shared_ptr<CTestA> m_test;
+};
+
 int main()
 {
+#if 0
 	std::shared_ptr<CTest> test = std::make_shared<CTest>();
 	std::cout<<"reference count: "<<test.use_count()<<std::endl;
 	{
 		std::shared_ptr<CTest> test1(test);
 		std::cout<<"reference count1: "<<test.use_count()<<std::endl;
 	}
+	
+	{
+		std::shared_ptr<CTestA> testA = std::make_shared<CTestA>();
+		testA->SetReference(test);
+		std::cout<<"reference count3: "<<test.use_count()<<std::endl;
+	}
 	std::cout<<"reference count2: "<<test.use_count()<<std::endl;
+#else
+	std::shared_ptr<CTestB> test;
+	{
+		std::shared_ptr<CTestA> testA = std::make_shared<CTestA>();
+		std::shared_ptr<CTestB> testB = std::make_shared<CTestB>();
+		testA->SetReference(testB);
+		testB->SetReference(testA);
+		test = testB;
+	}
+	std::cout<<"reference count2: "<<test.use_count()<<std::endl;
+#endif
 	return 0;
 }
 
