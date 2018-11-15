ee45533aa21b44c1382334cdcf773376c548f858
newbie1988 <312462392@qq.com>
2018-11-15 12:07:51 +0800

    add foreach for map

--- c11/foreach/main.cpp
+++ c11/foreach/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -28,8 +29,17 @@ int main()
 	}
 	std::cout<<std::endl;
 
-	std::for_each(std::begin(strs), std::end(strs), [&](std::string elem){ std::cout<<elem<<", ";});
+	std::for_each(std::begin(strs), std::end(strs), [](std::string elem){ std::cout<<elem<<", ";});
 	std::cout<<std::endl;	
 
+	std::map<std::string, int> persons;
+	persons["Jack"] = 12;
+	persons["Lily"] = 2;
+	persons["Mike"] = 5;
+	persons["Tim"] = 8;
+	for(auto& itr : persons)
+	{
+		std::cout<<"name: "<<itr.first<<", age: "<<itr.second<<std::endl;
+	}
 	return 0;
 }
